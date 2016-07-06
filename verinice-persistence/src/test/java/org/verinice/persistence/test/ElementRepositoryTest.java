eb407e420405455985ccc51485c7f5b8d0e59939
Ruth Motza <rm@sernet.de>
2016-07-06 14:34:57 +0200

    Adapt persistence-test to be executable with embedded database
    VN-1360
    
    The persistence testnow runs with h2-database. The test cases are
    adapted as well.

--- verinice-persistence/src/test/java/org/verinice/persistence/test/ElementRepositoryTest.java
+++ verinice-persistence/src/test/java/org/verinice/persistence/test/ElementRepositoryTest.java
@@ -21,7 +21,6 @@ package org.verinice.persistence.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,13 +29,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.verinice.persistence.ElementRepository;
 import org.verinice.persistence.PersistenceApplication;
-import org.verinice.persistence.PropertyRepository;
 import org.verinice.persistence.entities.CnATreeElement;
 
 
@@ -52,62 +47,34 @@ public class ElementRepositoryTest {
     @Autowired
     private ElementRepository elementRepository;
 
-    @Autowired
-    private PropertyRepository pRepository;
-
     private CnATreeElement element;
+    private CnATreeElement element2;
 
     @Before
     public void init() {
         element = new CnATreeElement();
-        // element.setUuid("eae74c67-b57b-4541-8de7-35cc43e93396");
-        // element.setType("incident_scenario");
-        // element.setUuid("abdb719c-e7b7-4c93-977c-55f3fb789e03");
-        // element.setType("bsimodel");
-        element.setUuid("f061407d-a60f-49ed-be3a-0b22ca703864");
-        // element = elementRepository.save(element);
+        element2 = new CnATreeElement();
+        element2.setUuid("eae74c67-b57b-4541-8de7-35cc43e93396");
+        element2.setType("incident_scenario");
+        element.setUuid("abdb719c-e7b7-4c93-977c-55f3fb789e03");
+        element.setType("bsimodel");
+        element = elementRepository.save(element);
+        element = elementRepository.save(element2);
     }
 
     @Test
     public void testFindOne() {
 
-        Pageable topTen = new PageRequest(0, 100);
-        Page<CnATreeElement> findAll = elementRepository.findAll(topTen);
-        findAll.forEach(elem -> LOG.debug(elem.toString()));
-        // Iterable<CnATreeElement> elementResult = elementRepository.findAll();
         CnATreeElement findByUuid = elementRepository.findByUuid(element.getUuid());
-        if (findByUuid == null) {
-            fail("element null");
-        }
-        // LOG.debug("result: " + (elementResult != null));
-        // if (elementResult != null) {
-            // LOG.debug(elementResult.iterator().next().toString());
-        // elementResult.forEach((element) -> LOG.debug(element.toString()));
-        // }
-        // check(findByUuid, element);
-        LOG.debug(findByUuid.toString());
-        // assertTrue(elementResult != null);
-
-        // Iterable<Property> findAll = pRepository.findAll();
-        // if (findAll != null) {
-        // Property next = findAll.iterator().next();
-        // if (next != null)
-        // LOG.debug(next.toString());
-        // // findAll.forEach(p -> {
-        // // if (p != null)
-        // // LOG.debug(p.toString());
-        // else
-        // LOG.debug("property null");
-        // // );
-        // } else
-        // LOG.debug("no properties found");
+        check(findByUuid, element);
+        findByUuid = elementRepository.findByUuid(element2.getUuid());
+        check(findByUuid, element2);
     }
 
     private void check(CnATreeElement elementResult, CnATreeElement element2) {
         assertTrue("element null", elementResult != null);
         assertEquals("wrong uuid", elementResult.getUuid(), element.getUuid());
-        // assertEquals("wrong type", elementResult.getType(),
-        // element.getType());
+        assertEquals("wrong type", elementResult.getType(), element.getType());
 
     }
 
