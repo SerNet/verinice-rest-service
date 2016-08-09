ca9b6593c2e34a29f38a3693fd6099f079c08666
Ruth Motza <rm@sernet.de>
2016-08-09 18:58:13 +0200

    Eliminate warnings in verinic-rest-service
    VN-1360
    
    There were some warnings wich indicaed unused classes. These are now
    removed to ensure warning-free projects.

--- verinice-persistence/src/test/java/org/verinice/persistence/test/ElementRepositoryTest.java
+++ verinice-persistence/src/test/java/org/verinice/persistence/test/ElementRepositoryTest.java
@@ -25,8 +25,6 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
@@ -45,7 +43,6 @@ import org.verinice.persistence.entities.CnaTreeElement;
 @ActiveProfiles("persistenceTest")
 public class ElementRepositoryTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ElementRepositoryTest.class);
     @Autowired
     private ElementRepository elementRepository;
 
