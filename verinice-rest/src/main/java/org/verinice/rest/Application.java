04361b87359058bf7a29eb68896a94d145182e94
Daniel Murygin <dm@sernet.de>
2016-08-23 13:25:28 +0200

    Make constructors public again in Spring Application classes

--- verinice-rest/src/main/java/org/verinice/rest/Application.java
+++ verinice-rest/src/main/java/org/verinice/rest/Application.java
@@ -36,7 +36,7 @@ import org.springframework.context.annotation.Configuration;
                 "org.verinice.service"})
 public class Application {
     
-    private Application() {
+    public Application() {
         super();
     }
 
