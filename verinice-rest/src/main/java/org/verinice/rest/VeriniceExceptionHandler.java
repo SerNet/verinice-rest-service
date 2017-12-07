e3a72e28c9299aeb438a3ca79bef49c6222eb984
Alexander Ben Nasrallah <an@sernet.de>
2017-12-07 16:12:58 +0100

    Add exception handling
    VN-1860

---
+++ verinice-rest/src/main/java/org/verinice/rest/VeriniceExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.verinice.rest;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.NoResultException;
+
+@ControllerAdvice
+public class VeriniceExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({ DataIntegrityViolationException.class, NoResultException.class })
+    protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
